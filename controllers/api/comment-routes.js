f06a6b118c4cfb7fbf6ed221c2e12a182c05dcda
themadtaco <aatrox48@gmail.com>
2022-03-01 19:49:00 -0600

    added comment posting and upvoting

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -11,17 +11,21 @@ router.get('/', (req, res) => {
 });
 
 router.post('/', (req, res) => {
-  // expects => {comment_text: "This is the comment", user_id: 1, post_id: 2}
+  // check the session
+  if (req.session) {
+    // expects => {comment_text: "This is the comment", user_id: 1, post_id: 2}
   Comment.create({
     comment_text: req.body.comment_text,
-    user_id: req.session.user_id,
-    post_id: req.body.post_id
+    post_id: req.body.post_id,
+    // use the id from the session
+    user_id: req.session.user_id
   })
     .then(dbCommentData => res.json(dbCommentData))
     .catch(err => {
       console.log(err);
       res.status(400).json(err);
     });
+  }
 });
 
 router.delete('/:id', (req, res) => {
