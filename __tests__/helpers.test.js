d568546391bb0dfff09779c89d4e886cca528236
themadtaco <aatrox48@gmail.com>
2022-03-02 21:11:47 -0600

    finished adding helpers

--- __tests__/helpers.test.js
+++ __tests__/helpers.test.js
@@ -1,7 +1,22 @@
-const {format_date} = require('../utils/helpers');
+const {format_date, format_plural, format_url} = require('../utils/helpers');
 
 test('format_date() returns a date string', () => {
     const date = new Date('2020-03-20 16:12:03');
 
     expect(format_date(date)).toBe('3/20/2020');
+});
+
+test('format_plural() returns a plural string', () => {
+    expect(format_plural('tiger', 2)).toBe('tigers');
+    expect(format_plural('lion', 1)).toBe('lion');
+});
+
+test('format_url() returns a simplified url string', () => {
+    const url1 = format_url('http://test.com/pag/1');
+    const url2 = format_url('https://www.coolstuff.com/acdefg/');
+    const url3 = format_url('https://www.google.com?q=hello');
+
+    expect(url1).toBe('test.com');
+    expect(url2).toBe('coolstuff.com');
+    expect(url3).toBe('google.com');
 });
